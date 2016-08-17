8d953f421387ceecb7244a658fb8c424478c32e3
Minh Truong <minh.truong@datatekniikka.fi>
2016-08-17 18:29:45 +0300

    Added check_memory plugin

---
+++ windows-plugins/check_memory.bat
@@ -0,0 +1,58 @@
+:: check_memory.bat plugin to icinga and nagios /MTr
+::
+::
+:: check_memory.bat %Warning %Critical%
+::
+:: 
+
+@echo off
+
+
+set /a Warning=90%
+set /a Critical=95%
+
+
+for /f "skip=1" %%p in ('wmic os get freephysicalmemory') do ( 
+  set FreeMemory=%%p
+  goto :done
+)
+:done
+for /f "skip=1" %%s in ('wmic os get TotalVisibleMemorySize') do ( 
+  set TotalMemory=%%s
+  goto :done
+)
+:done
+
+
+set /a UsedMemory=%TotalMemory% - %FreeMemory%
+set /a UsedMemoryPct=%UsedMemory% * 100 / %TotalMemory%
+
+set /a Warn=%TotalMemory% / 100 * %Warning%
+set /a Crit=%TotalMemory% / 100 * %Critical%
+
+set perf_data=used=%UsedMemory%KB;%Warn%;%Crit%;;%TotalMemory% free=%FreeMemory%KB;;;; total=%TotalMemory%KB;;;;
+
+
+if %UsedMemoryPct% geq %Critical% goto critical
+if %UsedMemoryPct% geq %Warning% goto warning
+if %UsedMemoryPct% lss %Warning% goto ok
+goto unknown
+
+
+
+
+
+:warning
+echo MEMORY WARNING - Free: %FreeMemory%KB;; Used: %UsedMemory%KB;; Total: %TotalMemory%KB;; ^| %perf_data%
+exit /b 1
+
+:critical
+echo MEMORY CRITICAL - Free: %FreeMemory%KB;;Used: %UsedMemory%KB;; Total: %TotalMemory%KB;; ^| %perf_data%
+exit /b 2
+
+:ok
+echo MEMORY OK - Free: %FreeMemory%KB;; Used: %UsedMemory%KB;; Total: %TotalMemory%KB;; ^| %perf_data%
+exit /b 0
+:unknown
+echo MEMORY UNKNOWN - Free: %FreeMemory%KB;; Used: %UsedMemory%KB;; Total: %TotalMemory%KB;; ^| %perf_data%
+exit /b 3
